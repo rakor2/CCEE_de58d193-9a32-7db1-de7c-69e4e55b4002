5ec766fd6bcbe15dbbfe12844d2c2aa2f17b3669
rakor2 <winster2207@gmail.com>
2025-06-06 11:42:15 +0300

    oopsie

--- ScriptExtender/Lua/Shared/LibLib/_init.lua
+++ ScriptExtender/Lua/Shared/LibLib/_init.lua
@@ -3,14 +3,7 @@ Ext.Require("Shared/LibLib/Helpers.lua")
 Ext.Require("Shared/LibLib/ImGui/_init.lua")
 
 
-Helpers.UserVars:Register('CCEE_Last', {
-    Server = true,
-    Client = true,
-    SyncToClient = true,
-    SyncToServer = true,
-    SyncOnTick = true,
-    SyncOnWrite = true,
-})
+
 
 -- Ext.Vars.RegisterUserVariable("NRD_Whatever", {
 --     Server = true,
--- ScriptExtender/Lua/Shared/_init.lua
+++ ScriptExtender/Lua/Shared/_init.lua
@@ -1,5 +1,13 @@
 Ext.Require("Shared/LibLib/_init.lua")
 Ext.Require("Shared/Tables.lua")
 
+Helpers.UserVars:Register('CCEE_Last', {
+    Server = true,
+    Client = true,
+    SyncToClient = true,
+    SyncToServer = true,
+    SyncOnTick = true,
+    SyncOnWrite = true,
+})
 
 -- DPrint(Helpers.Dice:Roll(1,10))
