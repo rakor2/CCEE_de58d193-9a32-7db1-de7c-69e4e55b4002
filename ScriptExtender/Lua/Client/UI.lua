3f171efeee2d9a8f91e2622342107fe871ce40c5
rakor2 <winster2207@gmail.com>
2025-06-06 11:39:50 +0300

    Test stuff

--- ScriptExtender/Lua/Client/UI.lua
+++ ScriptExtender/Lua/Client/UI.lua
@@ -14,6 +14,10 @@ PERHAPS FIX TAGS AND SCALING
 
 UI = {}
 Window = {}
+Skin = {}
+Tattoo = {}
+Tests = {}
+
 
 local OPENONRESETQUESTIONMARK = true
 
@@ -21,6 +25,9 @@ local OPENONRESETQUESTIONMARK = true
 function UI:Init()
     self.MCM = Window:CCEEMCM()
     self.Window = Window:CCEEWindow()
+    self.SkinColor =  Skin:Color()
+    self.TattooParams = Tattoo:Parameters()
+    self.Tests = Tests:Tests()
 end
 
 
@@ -68,32 +75,180 @@ function Window:CCEEWindow()
 
 
 
-    local picker = parent:AddColorPicker('Melanin color')
-    picker.OnChange = function()
-        ApplyParameters('head', 'MelaninColor', 'Vector3', picker.Color)
-        ApplyParameters('body', 'MelaninColor', 'Vector3', picker.Color)
-    end
+    function Skin:Color()
 
-    local slider = parent:AddSliderInt('tatindex', 1,0, tattooCount)
-    slider.OnChange = function()
-        ApplyParameters('head', 'TattooIndex', 'Scalar', slider.Value[1])
-    end
-
-
-    local picker2 = parent:AddColorPicker('xd2')
-    picker2.OnChange = function()
-        ApplyParameters('head', 'TattooColorB', 'Vector3', picker2.Color)
-    end
-
-    local slider2 = parent:AddSlider('tatint', 1,0,1)
-    slider2.OnChange = function()
-        ApplyParameters('head', 'TattooIntensity', 'Vector4', slider2.Value[1])
-    end
-
-
-    function ModVarsTest()
+        local skinColorCollapse = parent:AddCollapsingHeader('Skin color')
         
+        local picker = skinColorCollapse:AddColorPicker('Melanin color')
+        picker.OnChange = function()
+            for _, part in ipairs({'Head', 'Body', 'Genital', 'Tail', 'Horns'}) do
+                ApplyParameters(part, 'MelaninColor', 'Vector3', picker.Color)
+            end
+        end
+
+
+        local picker = skinColorCollapse:AddColorPicker('Melanin color')
+        picker.OnChange = function()
+            for _, part in ipairs({'Head', 'Body', 'Genital', 'Tail', 'Horns'}) do
+                ApplyParameters(part, 'MelaninColor', 'Vector3', picker.Color)
+            end
+        end
+
     end
+
+
+    function Tattoo:Parameters()
+
+        local fTatsColorCollapse = parent:AddCollapsingHeader('Face tattoo')
+
+        local slider = fTatsColorCollapse:AddSliderInt('Index', 1,0, tattooCount)
+        slider.OnChange = function()
+            ApplyParameters('Head', 'TattooIndex', 'Scalar', slider.Value[1])
+        end
+
+
+        local picker2 = fTatsColorCollapse:AddColorPicker('Color')
+        picker2.OnChange = function()
+            ApplyParameters('Head', 'TattooColorB', 'Vector3', picker2.Color)
+        end
+        
+        local slider2 = fTatsColorCollapse:AddSlider('Intensity', 1,0,1)
+        slider2.OnChange = function()
+            ApplyParameters('Head', 'TattooIntensity', 'Vector', slider2.Value[1])
+        end
+
+
+    end
+
+    function Tests:Tests()
+
+        local testParams = parent:AddCollapsingHeader('Tests')
+
+        function Body()
+
+            local testParamsBody = testParams:AddTree('Body')
+            local treeTestParams = testParamsBody:AddTree('Scalar')
+            local treeTestParams2 = testParamsBody:AddTree('Vec3')
+            local treeTestParams3 = testParamsBody:AddTree('Vec')
+    
+            function TestAllBodyScalarParameters()
+                for _,v in ipairs(Parameters.Body.Scalar) do
+                    local testSlider = treeTestParams:AddSliderInt(v, 0, -100, 100)
+                    testSlider.IDContext = v .. Ext.Math.Random(1,10000)
+                    testSlider.OnChange = function()
+                        for _, part in ipairs({'Body'}) do
+                            ApplyParameters(part, v, 'Scalar', testSlider.Value[1])
+                        end
+                    end
+                end
+            end
+    
+            function TestAllBodyVec3Parameters()
+                for _,v in ipairs(Parameters.Body.Vector3) do
+                    local testPicker = treeTestParams2:AddColorEdit(v)
+                    testPicker.IDContext = v .. Ext.Math.Random(1,10000)
+                    testPicker.OnChange = function()
+                        for _, part in ipairs({'Body'}) do
+                            ApplyParameters(part, v, 'Vector3', testPicker.Color)
+                        end
+                    end
+                end
+            end
+    
+            function TestAllBodyVecParameter()
+                for _,v in ipairs(Parameters.Body.Vector) do
+                    local testSlider2 = treeTestParams3:AddSlider(v)
+                    testSlider2.IDContext = v .. Ext.Math.Random(1,10000)
+                    testSlider2.OnChange = function()
+                        for _, part in ipairs({'Body'}) do
+                            ApplyParameters(part, v, 'Vector', testSlider2.Value[1])
+                        end
+                    end
+                end
+            end
+    
+
+            TestAllBodyScalarParameters()
+            TestAllBodyVec3Parameters()
+            TestAllBodyVecParameter()
+    
+        end
+
+       
+        function Head()
+
+            local testParamsHead = testParams:AddTree('Head')
+            local treeTestParams4 = testParamsHead:AddTree('Scalar')
+            local treeTestParams5 = testParamsHead:AddTree('Vec3')
+            local treeTestParams6 = testParamsHead:AddTree('Vec')
+    
+
+            function TestAllHeadScalarParameters()
+                for _,v in ipairs(Parameters.Head.Scalar) do
+                    local testSlider = treeTestParams4:AddSliderInt(v, 0, -100, 100)
+                    testSlider.IDContext = v .. Ext.Math.Random(1,10000)
+                    testSlider.OnChange = function()
+                        for _, part in ipairs({'Head'}) do
+                            ApplyParameters(part, v, 'Scalar', testSlider.Value[1])
+                        end
+                    end
+                end
+            end
+    
+            function TestAllHeadVec3Parameters()
+                for _,v in ipairs(Parameters.Head.Vector3) do
+                    local testPicker = treeTestParams5:AddColorEdit(v)
+                    testPicker.IDContext = v .. Ext.Math.Random(1,10000)
+                    testPicker.OnChange = function()
+                        for _, part in ipairs({'Head'}) do
+                            ApplyParameters(part, v, 'Vector3', testPicker.Color)
+                        end
+                    end
+                end
+            end
+    
+            function TestAllHeadVecParameter()
+                for _,v in ipairs(Parameters.Head.Vector) do
+                    local testSlider2 = treeTestParams6:AddSlider(v)
+                    testSlider2.IDContext = v .. Ext.Math.Random(1,10000)
+                    testSlider2.OnChange = function()
+                        for _, part in ipairs({'Head'}) do
+                            ApplyParameters(part, v, 'Vector', testSlider2.Value[1])
+                        end
+                    end
+                end
+            end
+
+
+            TestAllHeadScalarParameters()
+            TestAllHeadVec3Parameters()
+            TestAllHeadVecParameter()
+
+        end
+
+    
+        function Genital()
+        end
+
+
+        function Tail()
+        end
+
+
+        function Horns()
+            
+        end
+
+
+
+        Body()
+        Head()
+        Genital()
+        Tail()
+        Horns()
+
+    end
+   
 
 end
 
