66104235f9818212689f58a63dd4b4361aa57676
rakor2 <winster2207@gmail.com>
2025-08-11 09:47:57 +0300

    bs

--- ScriptExtender/Lua/Shared/_init.lua
+++ ScriptExtender/Lua/Shared/_init.lua
@@ -1,7 +1,6 @@
 Ext.Require("Shared/LibLib/_init.lua")
 Ext.Require("Shared/SharedHandlers.lua")
 Ext.Require("Shared/Tables.lua")
-Ext.Require("Shared/Paperdolls.lua")
 -- DPrint(Helpers.Dice:Roll(1,10))
 
 
