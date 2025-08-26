1d0b0a3033e9fe2a11f09cce20cd8af5a0c26c4a
rakor2 <winster2207@gmail.com>
2025-08-26 18:14:40 +0300

    bs

--- ScriptExtender/Lua/Client/_init.lua
+++ ScriptExtender/Lua/Client/_init.lua
@@ -2,27 +2,3 @@ Ext.Require("Client/ClientHandlers.lua")
 Ext.Require("Client/ClientListeners.lua")
 Ext.Require("Client/ClientRequests.lua")
 Ext.Require("Client/UI.lua")
-
-
-Test = {}
-
-
-function Test:CharVis()
-    local allCharVis = Ext.Resource.GetAll('CharacterVisual')
-    -- for k,v in pairs(allCharVis) do
-    --    DPrint(k)
-    --     DDump()
-    -- end
-
-    for i = 1, 1 do
-        DDump(Ext.Resource.Get(allCharVis[i],'CharacterVisual'))
-    end
-end
-
-
-
-
-
-
-
-
