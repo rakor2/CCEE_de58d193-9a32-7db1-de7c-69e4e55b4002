ce5e688d395be8eb4ce67990f15df01cd2f426a7
rakor2 <winster2207@gmail.com>
2025-12-28 22:40:23 +0300

    CCEE2
    Start of the rework
    Small refactor

---
+++ ScriptExtender/Lua/Client/1_UI2.lua
@@ -0,0 +1,14 @@
+function MainTab(p)
+
+
+    E.btnTest = p:AddButton('Hi!')
+    E.btnTest.OnClick = function (e)
+        DPrint('Hi!')
+    end
+
+
+
+
+
+
+end
---
+++ ScriptExtender/Lua/Client/2_ClientHandlers2.lua
@@ -0,0 +1,169 @@
+local function attachmentFound(entity, attachmentIndex, attachment)
+
+    DPrint('In attachmentFound')
+
+    --Special case for the head, since some authors don't assign skeleton slot or don't put Head in template name. Though head attachment seems to be always 2
+    if attachment == 'Head' then
+        if  entity.Visual.Visual.Attachments[attachmentIndex].Visual.VisualResource and
+            entity.Visual.Visual.Attachments[attachmentIndex].Visual.VisualResource.SkeletonSlot:lower():find(attachment:lower()) or
+            entity.Visual.Visual.Attachments[attachmentIndex].Visual.VisualResource.Template:lower():find(attachment:lower()) then
+            return true
+        end
+    end
+
+    if  entity.Visual.Visual.Attachments[attachmentIndex].Visual.VisualResource and
+        entity.Visual.Visual.Attachments[attachmentIndex].Visual.VisualResource.SkeletonSlot:lower():find(attachment:lower()) then
+        return true
+    end
+
+end
+
+
+
+
+---@param entity EntityHandle
+---@param attachment VisualAttachment | string
+---@return Visual[]
+function FindAttachment(entity, attachment)
+    if entity and entity.Visual and entity.Visual.Visual then
+            for attachmentIndex = 1, #entity.Visual.Visual.Attachments do
+
+                if attachment == 'Tail' then
+                    if attachmentFound(entity, attachmentIndex, attachment) then
+                        local TaleVisuals = entity.Visual.Visual.Attachments[attachmentIndex].Visual
+                        return {TaleVisuals}
+                    end
+
+
+                elseif attachment == 'Head' then
+                    if attachmentFound(entity, attachmentIndex, attachment) then
+                        local HeadVisuals = entity.Visual.Visual.Attachments[attachmentIndex].Visual
+                        return {HeadVisuals}
+                    end
+
+
+                elseif attachment == 'Piercing' then
+                    local PiercingVisuals = {}
+                    for q = 1, #entity.Visual.Visual.Attachments[2].Visual.Attachments do
+                        if  entity.Visual.Visual.Attachments[2].Visual.Attachments[q].Visual.VisualResource and
+                            entity.Visual.Visual.Attachments[2].Visual.Attachments[q].Visual.VisualResource.Slot:lower():find(attachment:lower())
+                        then
+                            for _,v in pairs(entity.Visual.Visual.Attachments[attachmentIndex].Visual.Attachments) do
+                                local Visuals = v.Visual
+                                table.insert(PiercingVisuals, Visuals)
+                                return PiercingVisuals
+                            end
+                        end
+                    end
+
+
+                elseif attachment == 'NakedBody' then
+                    local BodyVisuals = {}
+                    for attachmentIndex = 1, #entity.Visual.Visual.Attachments do
+                        if entity.Visual.Visual.Attachments[attachmentIndex].Visual.VisualResource then
+
+                        --Searching for the body attachment throught object's name to support regular body attachment and dummy body attachment for outfits
+                        --Yes, body attachment is always 1, but I need to support dummy one also
+                        for _, Objects in pairs(entity.Visual.Visual.Attachments[attachmentIndex].Visual.VisualResource.Objects) do
+                            if Objects.ObjectID:lower():find('body') then
+                                local Visuals = entity.Visual.Visual.Attachments[attachmentIndex].Visual
+                                table.insert(BodyVisuals, Visuals)
+                            end
+                        end
+
+                        -- if entity.Visual.Visual.Attachments[attachmentIndex].Visual.VisualResource.Template:lower():find('body') then
+                        --     local visuals = entity.Visual.Visual.Attachments[attachmentIndex].Visual
+                        --     table.insert(BodyVisuals, visuals)
+                        -- end
+
+                    end
+                end
+
+
+                elseif attachment == 'Hair' then
+                    local HairVisuals = {}
+                    for attachmentIndex = 1, #entity.Visual.Visual.Attachments do
+                        if attachmentFound(entity, attachmentIndex, attachment) then
+                            local Visuals = entity.Visual.Visual.Attachments[attachmentIndex].Visual
+                            table.insert(HairVisuals, Visuals)
+                        end
+                    end
+                    return HairVisuals
+
+
+                elseif  attachment == 'Wings' then
+                    if attachmentFound(entity, attachmentIndex, attachment) then
+                        local WingsVisuals = entity.Visual.Visual.Attachments[attachmentIndex].Visual
+                        return {WingsVisuals}
+                    end
+
+                else
+                    --Other attachments. Also sucks up outfit's parameters, since it is an attachment
+                    if attachmentFound(entity, attachmentIndex, attachment) then
+                        local OtherVisuals = entity.Visual.Visual.Attachments[attachmentIndex].Visual
+                        return {OtherVisuals}
+                    end
+            end
+        end
+    end
+end
+
+
+
+local function nameCheck(name, attachment, parameterType)
+    for _, existingName in ipairs(Parameters[attachment][parameterType]) do
+        if existingName == name then
+            return true
+        end
+    end
+end
+
+
+
+local function insertParameter(ActiveMaterial, attachment, parameterType)
+    if not parameterType then return end
+    if not ActiveMaterial.Material.Parameters[parameterType] then return end
+
+    for _, sp in ipairs(ActiveMaterial.Material.Parameters[parameterType]) do
+        local pn = sp.ParameterName
+        if not nameCheck(pn) then
+            table.insert(Parameters[attachment][parameterType], pn)
+        end
+    end
+
+end
+
+
+
+---Gets entity's (character's) all available meterial parameters
+---@param entity EntityHandle
+function BuildAllParametersTable(entity)
+
+    Parameters = {}
+    local entity = entity or _C()
+
+    for _, attachment in ipairs(AllAttachments) do
+        for _, parameterType in ipairs(AllParameterTypes) do
+
+            local Visuals = FindAttachment(entity, attachment)
+
+            if Visuals then
+
+                Parameters[attachment] = Parameters[attachment] or {}
+                Parameters[attachment][parameterType] = Parameters[attachment][parameterType] or {}
+
+                for _, visuals in pairs(Visuals) do
+                    if visuals and visuals.ObjectDescs then
+                        for od = 1, #visuals.ObjectDescs do
+                            local ActiveMaterial = visuals.ObjectDescs[od].Renderable.ActiveMaterial
+                            if ActiveMaterial and ActiveMaterial.Material then
+                                insertParameter(ActiveMaterial, attachment, parameterType)
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+    return Parameters
+end
